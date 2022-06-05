a38763e2ab2b723eb389657119e2eb5247a0e56b
CarlosRamirez01 <carlosramirez.viteri01@gmail.com>
2022-06-05 15:35:55 -0500

    feat: first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,23 +9,25 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+v7!wkrtnf)x(kp7&#%c!55k(o5yfgy7gh7uy908&7hl+y&z9%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
 
 
 # Application definition
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'anymail'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#Send emails
 
+ANYMAIL = {
+    "SENDGRID_API_KEY": env('SECRET_SENDGRID_KEY'),
+}
+
+EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
+DEFAULT_FROM_EMAIL = env('DEFAULT_EMAIL_BACKEND')
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
